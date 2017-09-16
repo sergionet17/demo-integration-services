a33269e17aebf5ab27020bf3aa70050f3e1048b3
Mona Lisa <sergionet17@gmail.com>
2017-09-16 15:06:42 -0500

    cambio con script

--- script_subida.sh
+++ script_subida.sh
@@ -4,9 +4,6 @@ git  add .
 
 git commit -m "cambio con script"
 
-git config --global user.name "Mona Lisa"
-
-git config --global user.passworsd "Mona Lisa"
 
 git push --set-upstream https://github.com/sergionet17/demo-integration-services  master
 
